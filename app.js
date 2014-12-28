77c6f83911864bdea992e00f0eafb092129f4be2
Bryan Garza <brygarza@gmail.com>
2014-12-27 17:59:02 -0800

    add initial code for parsing registration

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/test');
@@ -7,6 +8,8 @@ var db = mongoose.connection;
 var app = express();
 var pub = __dirname + '/public';
 app.use(express.static(pub));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.set('view engine', 'jade');
 
@@ -27,6 +30,12 @@ var users = [
 
 app.get('/', function (req, res) {
   res.render('index', { users: users })
+});
+
+app.post('/register', function (req, res) {
+  console.log(req.body.register.email);
+  console.log(req.body.register.password);
+  console.log(req.body.register.username);
 });
 
 var server = app.listen(3000, function() {
