15061376f6ed89a6d8435da064c716279d8072c9
Bryan Garza <brygarza@gmail.com>
2014-12-27 17:01:57 -0800

    Add semicolons

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-var express = require('express')
-var mongoose = require('mongoose')
+var express = require('express');
+var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/test')
-var db = mongoose.connection
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
 
-var app = express()
-var pub = __dirname + '/public'
-app.use(express.static(pub))
+var app = express();
+var pub = __dirname + '/public';
+app.use(express.static(pub));
 
-app.set('view engine', 'jade')
+app.set('view engine', 'jade');
 
 function User(name, email) {
   this.name = name
   this.email = email
-}
+};
 
 var users = [
   new User('bg', 'bg@bg.com'),
   new User('fc', 'fc@fc.com'),
   new User('su', 'su@su.com')
-]
+];
 
 app.get('/', function (req, res) {
   res.render('users', { users: users })
-})
+});
 
 var server = app.listen(3000, function() {
 
-  var host = server.address().address
-  var port = server.address().port
+  var host = server.address().address;
+  var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port)
 
-})
+});
