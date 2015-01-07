68a499bcbfc3759f7d8109c30df66ddd66e28013
Bryan Garza <brygarza@gmail.com>
2015-01-06 22:38:27 -0800

    Fix POST routes

--- config/passport.js
+++ config/passport.js
@@ -40,7 +40,7 @@ module.exports = function(passport) {
 
           newUser.local.email    = email;
           newUser.local.password = newUser.generateHash(password);
-          newUser.local.username = req.body.register.username;
+          newUser.local.username = req.body.username;
 
           // save the user
           newUser.save(function(err) {
@@ -55,6 +55,27 @@ module.exports = function(passport) {
 
     });
 
+  }));
+
+  passport.use('local-login', new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+  function(req, email, password, done) {
+    User.findOne({ 'local.email' : email }, function(err, user) {
+      if (err)
+        return done(err);
+
+      if(!user)
+        return done(null, false, req.flash('loginMessage', 'No user found.'));
+
+      if (!user.validPassword(password))
+        return done(null, false, req.flash('loginMessage', 'Wrong password.'));
+
+      // if all is ok
+      return done(null, user);
+    });
   }));
 
 };
--- routes/routes.js
+++ routes/routes.js
@@ -8,7 +8,11 @@ module.exports = function(app, passport) {
     res.render('login', { message: req.flash('loginMessage') });
   });
 
-  // app.post('/login', etc);
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/', // redirect to the secure profile section
+    failureRedirect : '/login', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  }));
 
   app.get('/register', function(req, res) {
     res.render('register', { message: req.flash('registerMessage') });
